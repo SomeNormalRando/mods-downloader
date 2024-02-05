4473e4e707619d3ff0a533aa58ae78869e3b1b31
Rando <67262768+SomeNormalRando@users.noreply.github.com>
2024-02-05 13:32:16 +0800

    fully working script - filled in mods_list.json - script now downloads dependencies for every mod automatically - made README.md clearer

--- README.md
+++ README.md
@@ -2,53 +2,58 @@
 
 0.00. make sure you are using a computer with Internet access
 
-0.42. if you're reading this on GitHub, download everything here (click on the `Code` button -> click on `Download ZIP`)
+0.20. if you're reading this on GitHub, download everything here (click on the `Code` button -> click on `Download ZIP`)
 
-0.51. open File Explorer and navigate to the location you downloaded the ZIP file in the previous step
+0.40. open File Explorer and navigate to the location you downloaded the ZIP file in the previous step
 
-0.69. unzip the ZIP file (on Windows, right-click on the ZIP file, then click on "Extract All")
+0.60. unzip the ZIP file (on Windows, right-click on the ZIP file, then click on "Extract All")
+
+0.69. open the unzipped folder. there likely is another folder with the same name inside it. open it and you should see the same files you saw on GitHub. copy the path (location) of the second folder (the one containing the files)
 
 0.99. make sure you have Python and pip installed (get them at https://www.python.org/ if you don't)
 
 1. open a console
-    
-    - if you're on Windows:
-        1) press the Windows key (it's beside the `alt` key on your keyboard) and the R key at the same time
-        2) type `cmd`
-        3) press `enter`
-    - if you're not just Google how to do it
+	- if you're on Windows:
+		1) press the Windows key (it's beside the `alt` key on your keyboard) and the R key at the same time
+		2) type `cmd`
+		3) press `enter`
+	- if you're not just Google how to do it
 
 2. change the working directory in your console
 
-    in your console, run this (replace `<PATH>` with the path (location) of the unzipped file in step 0.69):
-    ```
-    cd <PATH>
-    ```
+	In your console, run `cd <folder path>` (replace *`<folder path>`* with the path (location) of the unzipped folder in step 0.69)
 
-    FOR EXAMPLE, if the path of your unzipped file was `C:\Users\username\Downloads\mods-downloader-main`, you'd run
-    ```
-    cd C:\Users\username\Downloads\mods-downloader-main
-    ```
+	FOR EXAMPLE, if the path of your unzipped folder was `C:\Users\username\Downloads\mods-downloader-main\mods-downloader-main`, you'd run
+	```
+	cd C:\Users\username\Downloads\mods-downloader-main\mods-downloader-main
+	```
+
+	**After running `cd`, the text on the left side of your console should change to* `<folder path>`*. It should look something like this:*
+	```
+	C:\Users\username>cd C:\test\mods-downloader-main\mods-downloader-main
+	C:\test\mods-downloader-main\mods-downloader-main>
+	```
+	*If it doesn't and you're on Windows, your* `<folder path>` *is likely in another drive. If that's the case, run `cd /D` instead of `cd`.* 
 
 3. install dependencies
 
-    run this command in your console (copy-paste it):
-    ```
-    py -m pip install -r requirements.txt
-    ```
+	run this command in your console (copy-paste it):
+	```
+	py -m pip install -r requirements.txt
+	```
 
 4. run the script
 
-    1) double-click on `download.py`
+	1) double-click on `download.py` (in the folder in step 0.69)
 
-        **OR**
+		**OR**
 
-    2) run this command in your console:
+	2) run this command in your console:
 
-        ```
-        python download.py
-        ```
-    
-    when it prompts you, type `y` and press `Enter`
+		```
+		py download.py
+		```
+
+	when it prompts you, type `y` and press `Enter`
 
 After running the script, all the JAR files of the mods should be in the folder `OUTPUT_MODS`.
--- download.py
+++ download.py
@@ -1,10 +1,13 @@
 import os
+import pathlib
 import json
 # HTTP library
 import requests
 
 import logging
 logging.basicConfig(format="[%(asctime)s] %(message)s", datefmt="%H:%M:%S", level=logging.INFO)
+
+from typing import List, Tuple, Set
 
 
 # if True, no client-side only mods will be downloaded
@@ -14,32 +17,45 @@ MODS_LIST_FILE_PATH = "mods_list.json"
 MODS_OUTPUT_DIRECTORY = "OUTPUT_MODS"
 
 
-def get_modrinth_download_URL(slug, version_id):
-    response = requests.get(f"https://api.modrinth.com/v2/project/{slug}/version/{version_id}")
-
-    response_JSON = response.json()
-
-    return response_JSON["files"][0]["url"]
-
-def download_fromURL_toFile(URL, file_path):
+def download_fromURL_toFile(URL: str, file_path: str):
     response = requests.get(URL, stream=True)
     with open(file_path, "wb") as output_file:
         for chunk in response.iter_content(chunk_size=128):
             output_file.write(chunk)
 
+def modrinth_getVersionFromIdOrNumber(mod_name, mod_version_number):
+    api_response = requests.get(f"https://api.modrinth.com/v2/project/{mod_name}/version/{mod_version_number}")
+    return api_response.json()
 
-def download_from_mods_list(mods_list, output_directory_path):
-    for [mod_name, version_number, client_side_only] in mods_list:
-        if NO_CLIENT_SIDE is True and client_side_only is True:
+def modrinth_getProjectVersions(mod_ID):
+    api_response = requests.get(f"https://api.modrinth.com/v2/project/{mod_ID}/version?loaders=[\"fabric\"]&game_versions=[\"1.20.1\"]")
+    return api_response.json()
+
+def download_from_mods_list(
+        mods_list: List[Tuple[str, str, bool]],
+        downloaded_mods_IDs: Set[str],
+        dependency_IDs_unfiltered: Set[str]
+):
+    for [mod_name, mod_version_number, mod_client_side_only] in mods_list:
+        if NO_CLIENT_SIDE is True and mod_client_side_only is True:
             logging.info("(skipped mod, NO_CLIENT_SIDE is True)")
             continue
-        
-        URL = get_modrinth_download_URL(mod_name, version_number)
-        
-        logging.info(f"downloading {mod_name}, version {version_number}")
 
-        filename = URL.split("/")[-1]
-        download_fromURL_toFile(URL, f"{output_directory_path}/{filename}")
+        res = modrinth_getVersionFromIdOrNumber(mod_name, mod_version_number)
+
+        download_URL = res["files"][0]["url"]
+        filename = res["files"][0]["filename"]
+        logging.info(f"downloading {mod_name}, version {mod_version_number} ({filename})")
+        download_fromURL_toFile(download_URL, f"{MODS_OUTPUT_DIRECTORY}/{filename}")
+
+        mod_project_id = res["project_id"]
+        downloaded_mods_IDs.add(mod_project_id)
+
+        for dependency in res["dependencies"]:
+            if (dependency["dependency_type"] == "required"):
+                dependency_IDs_unfiltered.add(dependency["project_id"])
+
+    return (downloaded_mods_IDs, dependency_IDs_unfiltered)
 
 
 print(f"\nThis script will download the mods listed in `{MODS_LIST_FILE_PATH}` into the directory (folder) of `{MODS_OUTPUT_DIRECTORY}`.\n\nThe mods will be downloaded from Modrinth (to be more specific: the URLs of the mods will be fetched from the Modrinth API, then the mods' JAR files will be downloaded from those URLs).\n\nThis, of course, requires a stable Internet connection throughout the process.")
@@ -66,34 +82,68 @@ with open(MODS_LIST_FILE_PATH) as mods_list_file:
     if NO_CLIENT_SIDE is True:
         logging.info("NOTE: `NO_CLIENT_SIDE` is set to True, client-side only mods will not be downloaded")
 
-    # PERFORMANCE MODS
+    downloaded_mods_IDs = set()
+    dependency_IDs_unfiltered = set()
+
+    # ~ PERFORMANCE MODS
     perf_mods_list = mods_list["mods"]["performance"]
 
     print("\n")
-    logging.info(f"fetching download URLS of {len(perf_mods_list)} performance mods...\n")
+    logging.info(f"fetching download URLs of {len(perf_mods_list)} performance mods...\n")
 
-    download_from_mods_list(perf_mods_list, MODS_OUTPUT_DIRECTORY)
+    (downloaded_mods_IDs, dependency_IDs_unfiltered) = download_from_mods_list(
+        perf_mods_list, downloaded_mods_IDs, dependency_IDs_unfiltered
+    )
 
-    # UTILITY MODS
+    # ~ UTILITY MODS
     utility_mods_list = mods_list["mods"]["utility"]
 
     print("\n")
-    logging.info(f"fetching download URLS of {len(utility_mods_list)} utility mods...\n")
+    logging.info(f"fetching download URLs of {len(utility_mods_list)} utility mods...\n")
 
-    download_from_mods_list(utility_mods_list, MODS_OUTPUT_DIRECTORY)
+    (downloaded_mods_IDs, dependency_IDs_unfiltered) = download_from_mods_list(
+        utility_mods_list, downloaded_mods_IDs, dependency_IDs_unfiltered
+    )
 
-    # CONTENT MODS
+    # ~ CONTENT MODS
     content_mods_list = mods_list["mods"]["content"]
 
     print("\n")
-    logging.info(f"fetching download URLS of {len(content_mods_list)} content mods...\n")
+    logging.info(f"fetching download URLs of {len(content_mods_list)} content mods...\n")
 
-    download_from_mods_list(content_mods_list, MODS_OUTPUT_DIRECTORY)
+    (downloaded_mods_IDs, dependency_IDs_unfiltered) = download_from_mods_list(
+        content_mods_list, downloaded_mods_IDs, dependency_IDs_unfiltered
+    )
 
-    # DEPENDENCY MODS
-    dependencies_mods_list = mods_list["mods"]["dependencies"]
+    # ~ DEPENDENCY MODS
+    # remove dependencies that are in the mods list JSON file (they've already been downloaded)
+    dependency_IDs_filtered = dependency_IDs_unfiltered - downloaded_mods_IDs
 
     print("\n")
-    logging.info(f"fetching download URLS of {len(content_mods_list)} dependency mods...\n")
+    logging.info(f"fetching download URLs of dependency mods...\n")
 
-    download_from_mods_list(dependencies_mods_list, MODS_OUTPUT_DIRECTORY)
+    for dependency_mod_ID in dependency_IDs_filtered:
+        res = modrinth_getProjectVersions(dependency_mod_ID)
+
+        download_URL = res[0]["files"][0]["url"]
+        filename = res[0]["files"][0]["filename"]
+        logging.info(f"downloading {filename}")
+        download_fromURL_toFile(download_URL, f"{MODS_OUTPUT_DIRECTORY}/{filename}")
+
+        # dependencies of this dependency (if any)
+        d_dependencies = res[0]["dependencies"]
+        for d in d_dependencies:
+            project_id = d["project_id"]
+            if d["dependency_type"] == "required" and project_id not in downloaded_mods_IDs and project_id not in dependency_IDs_filtered:
+                r = modrinth_getProjectVersions(project_id)
+
+                download_URL = r[0]["files"][0]["url"]
+                filename = r[0]["files"][0]["filename"]
+                logging.info(f"downloading {filename}")
+                download_fromURL_toFile(download_URL, f"{MODS_OUTPUT_DIRECTORY}/{filename}")
+
+print("\n")
+print(f"done!\n\nall your mods should now be in {pathlib.Path(__file__).parent.resolve()}\\{MODS_OUTPUT_DIRECTORY}")
+
+print("\n")
+input("(press enter to close)\n")
